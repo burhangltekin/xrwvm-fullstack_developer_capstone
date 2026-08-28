2fdb35ba339757f8e3ca1c71e85546dc7e704ebb
burhangltekin <burhangltekin2@gmail.com>
2026-08-28 09:10:08 -0400

    Save

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,4 +1,5 @@
 # Uncomment the required imports before adding the code
+from .models import CarMake, CarModel
 
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
@@ -79,6 +80,17 @@ def register_request(request):
     else :
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # ...
 
